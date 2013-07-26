81fc8db7ba3a43cd2e584fd1a8a9ac672d0b9c86
Justin Collins <justin.collins@nrel.gov>
2013-07-26 15:46:21 -0600

    committing

--- src/DatabusMapredTest.java
+++ src/DatabusMapredTest.java
@@ -218,7 +218,9 @@ public class DatabusMapredTest extends Configured implements Tool
             RowImpl row = new RowImpl(colTree);
             row.setKey(key);
             
-            System.err.println("After Row is created, it has "+row.getColumns().size()+" columns");
+            com.alvazan.orm.api.z8spi.action.Column next = row.getColumns().iterator().next();
+            System.err.println("After Row is created, it has "+row.getColumns().size()+" columns [0]name is "+next.getName()+" value is "+next.getValue()+" timestamp is "+next.getTimestamp());
+
             String tableNameIfVirtual = DboColumnIdMeta.fetchTableNameIfVirtual(key);
     		System.err.println("z tableNameIfVirtual is "+tableNameIfVirtual);
 
