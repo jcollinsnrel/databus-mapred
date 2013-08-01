49707a5d5715de4b5757a7e73904ac587651579d
Justin Collins <justin.collins@nrel.gov>
2013-08-01 13:27:00 -0600

    committing

--- src/DatabusMapredTest.java
+++ src/DatabusMapredTest.java
@@ -125,7 +125,7 @@ public class DatabusMapredTest extends Configured implements Tool
 	    		URL location = src.getLocation();
 	            urls.add(location);
 	            log.info("******** location from codesource is "+location);
-	            File afile = new File(location.getPath());
+	            File afile = new File(location.getPath()+"lib/");
 	            log.info("******** afile absolute is "+afile.getAbsolutePath());
 	            log.info("******** afile tostring is "+afile);
 	            log.info("******** afile name is "+afile.getName());
