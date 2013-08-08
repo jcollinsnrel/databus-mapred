c98411a7aa288697512f9b2a9ba3578ef34c297f
Justin Collins <justin.collins@nrel.gov>
2013-08-08 15:08:59 -0600

    committing

--- src/TestClassloader.java
+++ src/TestClassloader.java
@@ -1,7 +1,12 @@
 import java.net.URL;
 import java.net.URLClassLoader;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TestClassloader extends URLClassLoader {
+
+	static final Logger log = LoggerFactory.getLogger(TestClassloader.class);
 
 	private String clname = "";
 
@@ -13,59 +18,46 @@ public class TestClassloader extends URLClassLoader {
 		clname = name;
 	}
 
-	protected Class<?> findClass(final String name)
-			throws ClassNotFoundException {
-		System.out.println("calling findClass with class " + name + ".  I am "
-				+ this);
+	protected Class<?> findClass(final String name) throws ClassNotFoundException {
+		log.debug("calling findClass with class " + name + ".  I am " + this);
 		Class<?> ret = super.findClass(name);
-		System.out.println("found class " + name + " I am " + name
-				+ " it's classloader is " + ret.getClassLoader());
+		log.debug("found class " + name + " I am " + name + " it's classloader is " + ret.getClassLoader());
 		return ret;
 	}
 
 	public Class<?> loadClass(String name) throws ClassNotFoundException {
-		System.out.println("calling loadClass with class " + name + ".  I am "
-				+ this);
+		log.debug("calling loadClass with class " + name + ".  I am " + this);
 		Class<?> ret = super.loadClass(name);
-		System.out.println("found class " + name + ".  I am " + this
-				+ " it's classloader is " + ret.getClassLoader()
-				+ " it's codesource it "
-				+ ret.getProtectionDomain().getCodeSource());
+		log.debug("found class " + name + ".  I am " + this + " it's classloader is " + ret.getClassLoader() + " it's codesource it " + ret.getProtectionDomain().getCodeSource());
 
 		return ret;
 	}
 
-	public Class<?> loadClass(String name, boolean resolve)
-			throws ClassNotFoundException {
-		System.out.println("calling loadClass boolean with class " + name
-				+ ".  I am " + this);
+	public Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+		log.debug("calling loadClass boolean with class " + name + ".  I am " + this);
 
 		// reverse the logic. try myself first, then delgate:
 		Class c = findLoadedClass(name);
 		if (c == null) {
 			try {
 				c = findClass(name);
-				System.out.println("Yes!  classloader" + this + " did have " + name);
-			} catch (ClassNotFoundException cnfe) {	
-				System.out.println("No!  classloader" + this + " didn't have " + name + " delegating to parent!");
+				log.debug("Yes!  classloader" + this + " did have " + name);
+			} catch (ClassNotFoundException cnfe) {
+				log.debug("No!  classloader" + this + " didn't have " + name + " delegating to parent!");
 				c = super.loadClass(name, resolve);
 			}
 		}
 		if (resolve) {
 			resolveClass(c);
 		}
-		System.out.println("found class " + name + ".  I am " + this
-				+ " it's classloader is " + c.getClassLoader()
-				+ " it's codesource it "
-				+ c.getProtectionDomain().getCodeSource());
+		log.debug("found class " + name + ".  I am " + this + " it's classloader is " + c.getClassLoader() + " it's codesource it " + c.getProtectionDomain().getCodeSource());
 
 		return c;
 	}
 
 	@Override
 	public String toString() {
-		return "TestClassloader named " + clname + " super: "
-				+ super.toString();
+		return "TestClassloader named " + clname + " super: " + super.toString();
 	}
 
 }
