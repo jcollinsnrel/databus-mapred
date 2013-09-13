d1bdb50c059135c41620700a4ac462ddd60c3952
Dean Hiller <dean@alvazan.com>
2013-09-13 11:36:46 -0600

    committing

--- src/DatabusMapredTest.java
+++ src/DatabusMapredTest.java
@@ -82,8 +82,8 @@ public class DatabusMapredTest extends Configured implements Tool
 	        //job.setNumReduceTasks(1);
 	
 	        ConfigHelper.setInputRpcPort(job.getConfiguration(), "9160");
-	        ConfigHelper.setInputInitialAddress(job.getConfiguration(), "a1.bigde.nrel.gov");
-	        //ConfigHelper.setInputInitialAddress(job.getConfiguration(), "sdi-prod-01.nrel.gov");
+	        //ConfigHelper.setInputInitialAddress(job.getConfiguration(), "a1.bigde.nrel.gov");
+	        ConfigHelper.setInputInitialAddress(job.getConfiguration(), "sdi-prod-01.nrel.gov");
 	        ConfigHelper.setInputPartitioner(job.getConfiguration(), "RandomPartitioner");
 	         // this will cause the predicate to be ignored in favor of scanning everything as a wide row
 	        ConfigHelper.setInputColumnFamily(job.getConfiguration(), KEYSPACE, COLUMN_FAMILY, false);
